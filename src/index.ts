9f12b91110d093d14c903f3fca41919dd660c303
Paulie-Aditya <paul.aditya304@gmail.com>
2025-03-21 16:18:27 +0530

    added message handler, but approach lacks auth

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
-import {server as WebSocketServer} from 'websocket';
+import {connection, server as WebSocketServer} from 'websocket';
 import http from 'http';
+import { UserManager } from './UserManager';
+import { IncomingMessage, SupportedMessage } from './messages';
+import { InMemoryStore } from './InMemoryStore';
 
-var server = http.createServer(function(request: any, response: any) {
+
+const server = http.createServer(function(request: any, response: any) {
     console.log((new Date()) + ' Received request for ' + request.url);
     response.writeHead(404);
     response.end();
 });
+const userManager = new UserManager();
+const store = new InMemoryStore()
 
 server.listen(8080, function() {
     console.log((new Date()) + ' Server is listening on port 8080');
@@ -37,16 +43,26 @@ wsServer.on('request', function(request) {
     var connection = request.accept('echo-protocol', request.origin);
     console.log((new Date()) + ' Connection accepted.');
     connection.on('message', function(message) {
+        //  add rate limiting logic here
         if (message.type === 'utf8') {
-            console.log('Received Message: ' + message.utf8Data);
-            connection.sendUTF(message.utf8Data);
-        }
-        else if (message.type === 'binary') {
-            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-            connection.sendBytes(message.binaryData);
+            try{
+                messageHandler(connection, JSON.parse(message.utf8Data));
+            }
+            catch(e){
+
+            }
+            // console.log('Received Message: ' + message.utf8Data);
+            // connection.sendUTF(message.utf8Data);
         }
     });
     connection.on('close', function(reasonCode, description) {
         console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
     });
 });
+
+function messageHandler(ws: connection, message : IncomingMessage){
+    if(message.type == SupportedMessage.JoinRoom){
+        const payload = message.payload;
+        userManager.addUser(payload.name, payload.userId, payload.roomId, ws);
+    }
+}
